0b1c1aaffd4210f0aaa2dc6f7577a2d636b8bf06
Mason <masonseaborn@gmail.com>
2025-10-02 14:45:12 -0500

    Update Noted.sh

--- Noted.sh
+++ Noted.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# The script Noted.sh was created by mason1600 on GitHub released with the license GNU General Public License v3.0
 # Echo command tells user to take a note
 echo 'Lets Take Notes!'
 # Read command reads note value into NOTE variable then puts it in this template with date to text document
